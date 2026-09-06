15cc63881d4c0eb8d099f1e4ff0db6e297da8f94
G-R3 <gr261212@gmail.com>
2026-09-05 21:35:51 -0400

    feat: fetch rates from API and add eth example

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import { computed, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
+const API_URL = "https://api.coinbase.com/v2/exchange-rates?currency=USD";
+
 const holdings = ref("10000");
 
-const btcRate = ref(0.00001249);
-const ethRate = ref(0.0003623);
+const rates = ref<Record<string, number>>({});
 
 const amount = computed(() => {
   const parsed = Number(holdings.value);
@@ -12,7 +13,26 @@ const amount = computed(() => {
 });
 
 const btcAmount = computed(() => {
-  return amount.value * 0.7 * btcRate.value;
+  return amount.value * 0.7 * rates.value.BTC;
+});
+
+const ethAmount = computed(() => {
+  return amount.value * 0.3 * rates.value.ETH;
+});
+
+async function getRates() {
+  try {
+    const response = await fetch(API_URL);
+    const { data } = await response.json();
+
+    rates.value = data.rates;
+  } catch (error) {
+    console.error("Error fetching rates:", error);
+  }
+}
+
+onMounted(() => {
+  getRates();
 });
 </script>
 
@@ -27,11 +47,10 @@ const btcAmount = computed(() => {
       :value="holdings"
     />
 
-    <span>{{ holdings }}</span>
-
     <section>
       <span>Buy</span>
       <span>BTC {{ btcAmount }}</span>
+      <span>ETH {{ ethAmount }}</span>
     </section>
   </section>
 </template>
